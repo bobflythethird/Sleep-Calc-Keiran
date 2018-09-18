eed3e59108f20990041938779aa2e6b6bdab0a53
bobflythethird <bobflythethird@gmail.com>
2018-09-18 14:12:13 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int birthYear, birthMonth, birthDay, crrntYear, crrntMonth, crrntDay, totDY, totDM, totDD;
+
+int main()
+{
+	cout << "Enter your year of birth." <<endl;
+	cin >> birthYear;
+	cout << "Enter your month of birth." <<endl;
+	cin >> birthMonth;
+	cout << "Enter your day of birth." <<endl;
+	cin >> birthDay;
+	cout << "Enter the current year." <<endl;
+	cin >> crrntYear;
+	cout << "Enter the current month." <<endl;
+	cin >> crrntMonth;
+	cout << "Enter the current day." <<endl;
+	cin >> crrntDay;
+	
+	
+}
+
